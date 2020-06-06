6736979a21f79aa34e81f2d13a64d27d0c05ba55
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-06-06 20:59:16 +0200

    more paths

--- egs/sprakbanken_nor/s5/path.sh
+++ egs/sprakbanken_nor/s5/path.sh
@@ -1,6 +1,6 @@
 export KALDI_ROOT=`pwd`/../../..
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH="$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/build/src/featbin:$KALDI_ROOT/build/src/fstbin:$PWD:$PATH"
+export PATH="$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/build/src/featbin:$KALDI_ROOT/build/src/fstbin:$KALDI_ROOT/build/src/gmmbin:$KALDI_ROOT/build/src/bin:$PWD:$PATH"
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
